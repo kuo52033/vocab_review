52108921c63b0fecdeb567adf42837accf25f06e
kuo52033 <tim31422@gmail.com>
2026-05-05 00:26:45 +0800

    fix: wire ios app target

---
+++ apps/ios/VocabReview/VocabReview/VocabReviewTests/VocabReviewTests.swift
@@ -0,0 +1,18 @@
+//
+//  VocabReviewTests.swift
+//  VocabReviewTests
+//
+//  Created by 郭峻維 on 2026/5/4.
+//
+
+import Testing
+
+struct VocabReviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
