8036398a397f960866971449692ff63f929d46e0
kuo52033 <tim31422@gmail.com>
2026-05-27 16:45:29 +0800

    fix: make auth token rollback idempotent

--- backend/migrations/00006_hash_auth_tokens.sql
+++ backend/migrations/00006_hash_auth_tokens.sql
@@ -9,7 +9,7 @@ ALTER TABLE magic_links ADD CONSTRAINT magic_links_email_key UNIQUE (email);
 -- +goose Down
 DELETE FROM magic_links;
 DELETE FROM sessions;
-ALTER TABLE magic_links DROP CONSTRAINT magic_links_email_key;
-ALTER TABLE magic_links DROP COLUMN created_at;
+ALTER TABLE magic_links DROP CONSTRAINT IF EXISTS magic_links_email_key;
+ALTER TABLE magic_links DROP COLUMN IF EXISTS created_at;
 ALTER TABLE magic_links RENAME COLUMN token_hash TO token;
 ALTER TABLE sessions RENAME COLUMN token_hash TO token;
